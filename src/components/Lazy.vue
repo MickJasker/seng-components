6bd1fbc8b05356c13f2a7613427dcfef4404db0e
Mick Jasker <mick.jasker@mediamonks.com>
2019-05-10 14:06:27 +0200

    Add documentation for the Lazy component

--- src/components/Lazy.vue
+++ src/components/Lazy.vue
@@ -15,19 +15,50 @@ export default class Lazy extends Vue {
 
   private observe: IntersectionObserver | undefined;
 
-  @Prop(String) readonly height!: string;
+  /**
+   * The minimum height of the component.
+   */
+  @Prop({ type: String, default: '200px' }) readonly height!: string;
 
+  /**
+   * The element that is used as the viewport for checking visiblity of
+   * the target. Must be the ancestor of the target. Defaults to the
+   * browser viewport if not specified or if ```null```.
+   */
   @Prop({ default: null }) readonly root!: HTMLElement | null;
 
+  /**
+   * Margin around the root. Can have values similar to the CSS margin
+   * property, e.g. ```"10px 20px 30px 40px"``` (top, right, bottom, left).
+   * The values can be percentages. This set of values serves to grow or
+   * shrink each side of the root element's bounding box before computing
+   * intersections. Defaults to all zeros.
+   */
   @Prop({ type: String, default: '0px' }) readonly rootMargin!: string;
 
-  @Prop({ type: Number, default: 0 }) readonly threshold!: number;
+  /**
+   * Either a single number or an array of numbers which indicate at what
+   * percentage of the target's visibility the observer's callback should
+   * be executed. If you only want to detect when visibility passes the
+   * ```50%``` mark, you can use a value of ```0.5```. If you want the
+   * callback to run every time visibility passes another 25%, you would
+   * specify the array ```[0, 0.25, 0.5, 0.75, 1]```. The default is ```0```
+   * (meaning as soon as even one pixel is visible, the callback will be run).
+   * A value of ```1.0``` means that the threshold isn't considered passed
+   * until every pixel is visible.
+   */
+  @Prop({ type: [Number, Array], default: 0 }) readonly threshold!: number | number[];
 
-  @Watch('inView', { immediate: true })
   setViewStateEvent(val: boolean): void {
     if (val === true) {
+      /**
+       * When element enters the view.
+       */
       this.$emit('enters-view');
     } else {
+      /**
+       * When element leaves the view.
+       */
       this.$emit('leaves-view');
     }
   }
@@ -41,6 +72,7 @@ export default class Lazy extends Vue {
     this.observe = new IntersectionObserver(
       (entries: IntersectionObserverEntry[]) => {
         entries.map((entry: IntersectionObserverEntry) => {
+          this.setViewStateEvent(entry.isIntersecting);
           if (!isPassed) {
             this.inView = entry.isIntersecting;
           } else if (this.inView === false) {
